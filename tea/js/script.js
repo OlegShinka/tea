2d5e9ab5c0f26eba01d51dd3b11c30ad34db07a5
Oleg Shinka <volkonxxl@gmail.com>
2025-09-02 22:58:40 +0300

    add backdrop

--- tea/js/script.js
+++ tea/js/script.js
@@ -1,11 +1,21 @@
 const Menu_open = document.querySelector(".menu__btn");
 const Menu_close = document.querySelector(".mobile-menu__btn-close");
 const Menu_list = document.querySelector(".header__mobile-menu");
+const Backdrop = document.querySelector(".backdrop");
 
 Menu_open.addEventListener("click", () => {
   Menu_list.classList.add("header__mobile-menu--isOpen");
+  Backdrop.classList.add("isVisible");
 });
 
 Menu_close.addEventListener("click", () => {
   Menu_list.classList.remove("header__mobile-menu--isOpen");
+  Backdrop.classList.remove("isVisible");
+});
+
+// close Menu to click backdrop
+
+Backdrop.addEventListener("click", () => {
+  Menu_list.classList.remove("header__mobile-menu--isOpen");
+  Backdrop.classList.remove("isVisible");
 });
